20e4dc58e7f9abb17c2ce5dd06a19c41732a363e
vcho1958 <gusckd57@gmail.com>
2021-08-28 18:59:52 +0900

    add react-calendar

--- src/App.js
+++ src/App.js
@@ -1,44 +1,78 @@
 
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import classnames from 'classnames';
 
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+const maxDates = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-class Cell extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      day: props.day,
-      holiday: props.holiday,
+function createCells(date) {
+  const cells = [];
+  let maxDate = maxDates[date.getMonth()];
+  let startDay = new Date(date.getFullYear(), date.getMonth(), 1);
+  startDay = startDay.getDay();
+  if (date.getMonth() === 1) {
+    if ((date.getFullYear() % 4 === 0 && date.getFullYear() % 100 !== 0) || date.getFullYear() % 400 === 0) {
+      maxDate = 29;
     }
   }
-  render() {
-    return <div class="calender-cell">{this.state.day}</div>;
+  for (let i = 0, j = 1; i < 42; i++) {
+    if (i < startDay || i >= maxDate + startDay)
+      cells.push(<div key={i} className="calendar-cell empty"></div>)
+    else {
+      cells.push(<div key={i} className={classnames('calendar-cell', { 'selected-date': j === date.getDate() })
+      }> {j}</div >);
+      j++;
+    }
   }
+  for (let i = 1; i <= maxDate; i++) {
+
+  }
+  for (let i = cells.length; i < 42; i++) {
+    cells.push(<div key={i + 1} className="calendar-cell empty"></div>)
+  }
+
+  return cells;
 }
-class Calender extends React.Component {
-  days = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
-  maxDates = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  constructor(props) {
-    super(props);
-    this.state = {
-      today: new Date(),
-      month: new Date().getMonth(),
-    }
-    // this.setState({ month: new Date(), selected: new Date })
 
-  };
-  generateCalendar() {
-    const cells = [];
-    for (let i = 1; i <= this.maxDates[this.state.month]; i++) {
-      cells.push(<Cell day={i} holiday={true} />)
-    }
-    return cells;
-  }
-  render() {
-    return <div class="calender">{
-      this.generateCalendar()
-    }</div>;
-  }
+function createColumns() {
+  const columns = [];
+  for (let i = 0; i < 7; i++) { columns.push(<div key={i} className="calender-column">{days[i].substr(0, 3).toLocaleUpperCase()}</div>); }
+  return columns;
 }
+function Calender() {
+  const date = new Date();
+  const [selectedDate, setSelectedDate] = useState(date);
+  const [cells, setCells] = useState(createCells(selectedDate));
+  const columns = createColumns();
+  const [month, setMonth] = useState(date.getMonth());
+  useEffect(() => {
+    if (month !== selectedDate.getMonth()) {
+      setCells(createCells(selectedDate));
+      setMonth(selectedDate.getMonth());
+    }
+  }, [month, selectedDate])
+
+  const prev = () => {
+    setSelectedDate(new Date(selectedDate.getFullYear(), selectedDate.getMonth() - 1, 1));
+
+  }
+  const next = () => {
+    setSelectedDate(new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 1));
+  }
+  return (<>
+    <section className="calendar-header">
+      <button className="prevMonth" onClick={prev}>Previous</button>
+      <h1>{months[selectedDate.getMonth()]}</h1>
+      <button className="nextMonth" onClick={next}>Next</button>
+    </section>
+    <section className="calendar-main">
+      {columns}{cells}
+    </section>
+  </>);
+}
+
+
 
 export default Calender;
